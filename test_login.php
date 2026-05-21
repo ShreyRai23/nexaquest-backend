882efecadc923116af4bcdf8efb6657c68a06fcd
Shreyr69 <shreyrai69@gmail.com>
2026-05-21 14:41:12 +0530

    Remove test scripts

--- test_login.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-$req = Illuminate\Http\Request::create('/api/auth/login', 'POST', ['email' => 'peter@gmail.com', 'password' => 'password123']);
-$res = app()->handle($req);
-echo $res->getContent();
